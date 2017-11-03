d5f30e4300098a782ee4c0ff890cfdbc5c0f6a9c
krzychu124 <krzychu124@gmail.com>
2017-11-03 21:30:26 +0100

    form update

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchformComponent } from './searchform/searchform.component';
 import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
