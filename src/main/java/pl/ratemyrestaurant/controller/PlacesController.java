badd48499d94643f9926eae2878f0c887dc37920
krzychu124 <krzychu124@gmail.com>
2017-11-06 05:27:22 +0100

    fixed mapping

--- src/main/java/pl/ratemyrestaurant/controller/PlacesController.java
+++ src/main/java/pl/ratemyrestaurant/controller/PlacesController.java
@@ -1,15 +1,37 @@
 package pl.ratemyrestaurant.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class PlacesController {
 
-    @GetMapping(value = "/getPlacesInRadius",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getPlacesInRadius(@RequestParam double lng,@RequestParam double lat,@RequestParam double radius, @RequestParam String type){
-        return lng+" "+lat+" "+radius+" "+type;
+//    @Secured("ROLE_ADMIN")
+    @GetMapping(value = "/getPlacesInRadius")
+    public String getPlacesInRadius(@RequestParam double lng, @RequestParam double lat, @RequestParam double radius, @RequestParam String type) {
+        ObjectMapper mapper= new ObjectMapper();
+        Map<String, Object> map = new HashMap<>();
+        map.put("lng", lng);
+        map.put("lat", lat);
+        map.put("radius", radius);
+        map.put("type", type);
+        String string = "";
+        try {
+            string = mapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return string;
     }
 }
