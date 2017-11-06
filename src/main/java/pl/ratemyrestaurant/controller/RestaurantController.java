22b7c554fcc06193bc0bfbde40654b622c2842a1
krzychu124 <krzychu124@gmail.com>
2017-11-06 05:27:05 +0100

    fixed mapping

--- src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java
+++ src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java
@@ -3,11 +3,13 @@ package pl.ratemyrestaurant.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.ratemyrestaurant.dto.RestaurantDTO;
 import pl.ratemyrestaurant.service.RestaurantService;
 
 @RestController
+@RequestMapping("/restaurant")
 public class RestaurantController {
 
     private RestaurantService restaurantService;
