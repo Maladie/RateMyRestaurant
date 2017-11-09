d7cca857b92e9142ca95b0b2eb6d1169028948c2
Maladie <mariusz.maks.szymanski@gmail.com>
2017-11-09 18:40:35 +0100

    Merge branches 'MalBranch' and 'master' of https://github.com/Maladie/RentMyRestaurant into MalBranch
    # Conflicts:
    #	src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java

--- src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java
+++ src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java
@@ -21,18 +21,20 @@ public class RestaurantController {
         this.restaurantService = restaurantService;
     }
 
+    //działa
     @GetMapping(value = "/{restaurantId}/ingredients", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<IngredientDTO> getIngredientsByThumbs(@PathVariable String restaurantId,
                                                       @RequestParam (required = false) String orderBy){
         return restaurantService.getIngredientsByThumbs(restaurantId, orderBy);
     }
-
+    //działa
     @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<RestaurantDTO> persistRestaurant(@RequestBody RestaurantDTO restaurantDTO){
         restaurantService.addOrUpdateRestaurant(restaurantDTO);
         return new ResponseEntity<>(restaurantDTO, HttpStatus.CREATED);
     }
 
+    //prawdopodobnie działa
     @GetMapping("/restaurants/ingredient/{ingredientName}")
     public List<RestaurantDTO> getAllRestaurantsContainingIngredient(@PathVariable String ingredientName){
         return restaurantService.getRestaurantsContainingIngredient(ingredientName);
