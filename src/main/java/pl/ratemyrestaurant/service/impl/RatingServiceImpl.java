fe7d2fbcb4b26cfb64b214e6315dc840cf9147b1
Jakub <jakkabza@gmail.com>
2017-11-10 19:44:59 +0100

    sorted search by ingredient added

--- src/main/java/pl/ratemyrestaurant/service/impl/RatingServiceImpl.java
+++ src/main/java/pl/ratemyrestaurant/service/impl/RatingServiceImpl.java
@@ -2,6 +2,8 @@ package pl.ratemyrestaurant.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.ratemyrestaurant.dto.RatingDTO;
+import pl.ratemyrestaurant.mappers.RatingToRatingDTOMapper;
 import pl.ratemyrestaurant.model.Ingredient;
 import pl.ratemyrestaurant.model.Rating;
 import pl.ratemyrestaurant.model.Restaurant;
@@ -9,7 +11,10 @@ import pl.ratemyrestaurant.model.Thumb;
 import pl.ratemyrestaurant.repository.RatingRepository;
 import pl.ratemyrestaurant.service.RatingService;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class RatingServiceImpl implements RatingService {
@@ -29,5 +34,20 @@ public class RatingServiceImpl implements RatingService {
         Rating rating = new Rating(restaurant, ingredient, new Thumb());
         ratingRepository.save(rating);
         return rating;
+    }
+
+    @Override
+    public List<RatingDTO> retrieveRatingsOfIngredientInRestaurants(String ingredientName) {
+        List<Rating> ratings = ratingRepository.findByIngredient_Name(ingredientName);
+        return ratings.stream().sorted((r1, r2) -> {
+            return Float.compare(countThumbPercentage(r2.getThumb()), countThumbPercentage(r1.getThumb()));
+        }).map(r -> RatingToRatingDTOMapper.mapRatingToRatingDto(r)).collect(Collectors.toList());
+
+    }
+
+    private float countThumbPercentage(Thumb thumb){
+        float a = (float)thumb.getThumbsDown();
+        float b = (float)thumb.getThumbsDown();
+        return a / (a+b);
     }
 }
