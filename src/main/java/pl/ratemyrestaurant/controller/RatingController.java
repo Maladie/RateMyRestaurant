4a1f845f2f0c83cd29be18e86793bc69ef329682
Jakub <jakkabza@gmail.com>
2017-11-10 18:22:31 +0100

    Merge branch 'master' of github.com:Maladie/RateMyRestaurant into hassan
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/pl/ratemyrestaurant/controller/RatingController.java
+++ src/main/java/pl/ratemyrestaurant/controller/RatingController.java
@@ -1,11 +1,10 @@
 package pl.ratemyrestaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.ratemyrestaurant.model.Rating;
-import pl.ratemyrestaurant.service.impl.RatingService;
+import pl.ratemyrestaurant.service.RatingService;
+import pl.ratemyrestaurant.service.impl.RatingServiceImpl;
 
 @RestController
 @RequestMapping("/rating")
