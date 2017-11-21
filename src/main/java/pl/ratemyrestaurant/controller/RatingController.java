38ac57fd3627ca7f02fb8bdc8b618d0850482b64
krzychu124 <krzychu124@gmail.com>
2017-11-22 00:53:08 +0100

    changed rateIngredient endpoint and implementation

--- src/main/java/pl/ratemyrestaurant/controller/RatingController.java
+++ src/main/java/pl/ratemyrestaurant/controller/RatingController.java
@@ -2,17 +2,17 @@ package pl.ratemyrestaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.ratemyrestaurant.dto.RatingDTO;
 import pl.ratemyrestaurant.dto.RestaurantPIN;
-import pl.ratemyrestaurant.model.Vote;
 import pl.ratemyrestaurant.service.RatingService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/rating")
+@RequestMapping(value = "/rating", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RatingController {
 
     private RatingService ratingService;
@@ -28,20 +28,13 @@ public class RatingController {
         return ratingService.retrieveRatingsOfIngredientInRestaurants(ingredientName, restaurantsFound);
     }
 
-    @PostMapping(value = "/rate")
-    public ResponseEntity<RatingDTO> rateIngredient(@RequestBody Vote vote) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        RatingDTO rating = new RatingDTO();
-        if (vote != null) {
-            rating = ratingService.addOrUpdateRating(vote);
-            status = HttpStatus.ACCEPTED;
+    @PutMapping(value = "/{ratingID}")
+    public ResponseEntity<RatingDTO> rateIngredientOfRatingID(@PathVariable Long ratingID, @RequestBody Boolean upVote) {
+        RatingDTO rating = null;
+        if (upVote != null) {
+            rating = ratingService.rateIngredient(ratingID, upVote);
         }
+        HttpStatus status = rating != null ? HttpStatus.ACCEPTED : HttpStatus.BAD_REQUEST;
         return new ResponseEntity<>(rating, status);
-    }
-
-    //? rateID instead of restaurantID, should be added to restaurant controller
-    @PostMapping(value = "/{restaurantId}/rate")
-    public ResponseEntity<RatingDTO> addIngredientRating(@PathVariable("restaurantId") String restaurantId, @RequestBody Vote vote){
-       return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
