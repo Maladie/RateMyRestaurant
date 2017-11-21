70c65c035efedf8e272588e0cef1826ab9396934
krzychu124 <krzychu124@gmail.com>
2017-11-22 00:50:18 +0100

    moved related endpoints from ratingController

--- src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java
+++ src/main/java/pl/ratemyrestaurant/controller/RestaurantController.java
@@ -9,7 +9,7 @@ import pl.ratemyrestaurant.dto.RatingDTO;
 import pl.ratemyrestaurant.dto.RestaurantDTO;
 import pl.ratemyrestaurant.dto.RestaurantPIN;
 import pl.ratemyrestaurant.model.UserSearchCircle;
-import pl.ratemyrestaurant.model.Vote;
+import pl.ratemyrestaurant.service.RatingService;
 import pl.ratemyrestaurant.service.RestaurantService;
 
 import java.util.HashSet;
@@ -20,10 +20,12 @@ import java.util.Set;
 public class RestaurantController {
 
     private RestaurantService restaurantService;
+    private RatingService ratingService;
 
     @Autowired
-    public RestaurantController(RestaurantService restaurantService) {
+    public RestaurantController(RestaurantService restaurantService, RatingService ratingService) {
         this.restaurantService = restaurantService;
+        this.ratingService = ratingService;
     }
 
     //temp. disabled
@@ -34,7 +36,7 @@ public class RestaurantController {
 
     //? unused currently
     @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<RestaurantDTO> persistRestaurant(@RequestBody RestaurantDTO restaurantDTO){
+    public ResponseEntity<RestaurantDTO> persistRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
         restaurantService.addOrUpdateRestaurant(restaurantDTO);
         return new ResponseEntity<>(restaurantDTO, HttpStatus.CREATED);
     }
@@ -48,26 +50,40 @@ public class RestaurantController {
     //TODO refactor
     //? Refactor idea
     @GetMapping(value = "/searchInRadius/{foodType}")
-    public Set<RestaurantDTO> getRestaurantsInRadiusByFoodType(@PathVariable String foodType){
+    public Set<RestaurantDTO> getRestaurantsInRadiusByFoodType(@PathVariable String foodType) {
         return new HashSet<>(); //TODO
     }
 
     //? Refactor response type from pin to regular restaurant
     //TODO add parameters validator here or in service
     @GetMapping(value = "/areaSearch")
-    public Set<RestaurantPIN> getRestaurantInRadius(@RequestParam double lng, @RequestParam double lat, @RequestParam double radius, @RequestParam(required = false) String type){
+    public Set<RestaurantPIN> getRestaurantInRadius(@RequestParam double lng, @RequestParam double lat, @RequestParam double radius, @RequestParam(required = false) String type) {
         UserSearchCircle userSearchCircle = new UserSearchCircle(lat, lng, radius);
         return restaurantService.retrieveRestaurantsInRadius(userSearchCircle);
     }
 
     @GetMapping(value = "/{restaurantID}")
-    public RestaurantDTO getRestaurantDetails(@PathVariable String restaurantID){
+    public RestaurantDTO getRestaurantDetails(@PathVariable String restaurantID) {
         return restaurantService.getOrRetrieveRestaurantDTOByID(restaurantID);
     }
 
-    //TODO impl.
-    @PostMapping(value = "/{restaurantId}/rate")
-    public ResponseEntity<RatingDTO> addIngredientRating(@PathVariable("restaurantId") String restaurantId, @RequestBody Vote vote){
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    //TODO ? /{restaurantId}/ingredientRating/
+    @PostMapping(value = "/{restaurantId}/ingredientRatings")
+    public ResponseEntity<RatingDTO> addIngredientRating(@PathVariable("restaurantId") String restaurantId, @RequestBody Long ingredientID) {
+        if (!restaurantService.isRestaurantInDB(restaurantId)) {
+            restaurantService.addOrUpdateRestaurant(restaurantService.getOrRetrieveRestaurantDTOByID(restaurantId));
+        }
+        RatingDTO ratingDTO = ratingService.addNewIngredientRating(restaurantId, ingredientID);
+        return new ResponseEntity<>(ratingDTO, ratingDTO != null ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping(value = "/{restaurantId}/ingredientRatings/{ingredientId}")
+    public ResponseEntity<RatingDTO> addIngredientRating(@PathVariable("restaurantId") String restaurantId, @PathVariable("ingredientId") Long ingredientId, @RequestBody Boolean upVote) {
+        RatingDTO rating = null;
+        if (upVote != null) {
+           rating = ratingService.rateIngredient(restaurantId, ingredientId, upVote);
+        }
+        HttpStatus status = rating != null ? HttpStatus.ACCEPTED : HttpStatus.BAD_REQUEST;
+        return new ResponseEntity<>(rating, status);
     }
 }
