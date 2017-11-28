3fc832779b3c2c614afe5488aed5a7d8ce853f05
krzychu124 <krzychu124@gmail.com>
2017-11-28 02:06:19 +0100

    test util method

---
+++ src/test/pl/ratemyrestaurant/utils/TestUtils.java
@@ -0,0 +1,14 @@
+package pl.ratemyrestaurant.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestUtils {
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
