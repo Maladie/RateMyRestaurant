f3674b5551320a37234c9a77f54c01712b4dfea1
krzychu124 <krzychu124@gmail.com>
2017-12-03 19:15:32 +0100

    add use ssl param

--- src/main/java/pl/ratemyrestaurant/config/MainConfig.java
+++ src/main/java/pl/ratemyrestaurant/config/MainConfig.java
@@ -16,7 +16,7 @@ public class MainConfig {
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +"?sslmode=require";
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
